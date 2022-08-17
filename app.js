1bcceccc886019846d083780335e30f018e32dbe
SENSA0907 <83896771+SENSA0907@users.noreply.github.com>
2022-08-17 19:17:53 +0530

    adding ratelimit middleware

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const MOCK_DATA = require("./mock");
 const express = require("express");
-const morgan = require('morgan');
-const responseTime = require('response-time')
-
-const router = express.Router();
+const morgan = require("morgan");
+const responseTime = require("response-time");
+const rateLimit = require('express-rate-limit')
 
 const data = MOCK_DATA.products;
 const PORT = 3001;
@@ -13,27 +12,45 @@ const app = express();
 // it is an example of express middleware
 // to add any middleware, make use of app.use from express
 
-
 // own middleware
 
 const customMiddleware = (req, res, next) => {
   console.log("This is coustom middleware");
   req.requestedTime = Date.now();
-  if (req.query.isAdmin == 'false') {
-    return res.status(404).json({'message': "you are not authorized to hit this route"})
+  if (req.query.isAdmin == "false") {
+    return res
+      .status(404)
+      .json({ message: "you are not authorized to hit this route" });
   }
   next();
-}
-// 1st middle 
+};
+// 1st middle
 app.use(customMiddleware);
 // 2nd middle ware
 app.use(express.json());
 // 3rd middleware
 app.use(express.urlencoded({ extended: true }));
 // 4th middleware
-app.use(morgan("dev"))
-// 5th middleware
-app.use(responseTime())
+app.use(morgan("dev"));
+
+// Rate Limiter
+const limiter = rateLimit({
+	windowMs: 10 * 1000, // 10 sec - have to give in milliseconds
+	max: 1, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: true, // Disable the `X-RateLimit-*` headers
+  message: "Please try after some time"
+})
+// This app.use is used to set rate limit middleware for all the routes below it
+app.use(limiter);
+
+const oneLimit = rateLimit({
+	windowMs: 15 * 10 * 1000, // 10 sec - have to give in milliseconds
+	max: 1, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
 
 const getProducts = (req, res) => {
   console.log(req.query, req.params);
@@ -42,9 +59,9 @@ const getProducts = (req, res) => {
     results: data.length,
     requestedAt: req.requestedTime,
     respondedAt: Date.now(),
-    timeTaken: Date.now() - req.requestedTime
+    timeTaken: Date.now() - req.requestedTime,
   });
-}
+};
 
 const postProduct = (req, res) => {
   // console.log(req.body);
@@ -52,7 +69,7 @@ const postProduct = (req, res) => {
   res.status(201).json({
     data: req.body,
   });
-}
+};
 
 const deleteProduct = (req, res) => {
   console.log(req.query, req.params.productId);
@@ -70,25 +87,24 @@ const deleteProduct = (req, res) => {
       message: "Requested Id is not found",
     });
   }
-}
+};
 
 const updateProduct = (req, res) => {
   console.log(req.query, req.params.productId);
   const index = data.findIndex((elem) => {
-      return elem.id == req.params.productId;
+    return elem.id == req.params.productId;
   });
   if (index !== -1) {
-      data[index] = {...data[index], ...req.body};
-      res.status(204).json({
-          message: "Updated"
-      })
+    data[index] = { ...data[index], ...req.body };
+    res.status(204).json({
+      message: "Updated",
+    });
   } else {
-      res.json({
-         message: "Requested data not found"
-        });
+    res.json({
+      message: "Requested data not found",
+    });
   }
-  
-}
+};
 
 const getProdct = (req, res) => {
   console.log(req.query, req.params.productId);
@@ -99,17 +115,21 @@ const getProdct = (req, res) => {
     data: filteredProduct ? filteredProduct : "No Data Found",
     requestedAt: Date.now(),
   });
-}
+};
 
-// since app.use is used , it can listen for GET POST and all other methods
-app.get("/api/v1/products", getProducts);
-app.post("/api/v1/products", postProduct);
-// router.post('/api/v1/products',postProduct).get('/api/v1/products',getProducts)
-
-
-app.delete(`/api/v1/products/:productId`, deleteProduct);
+/* app.get("/api/v1/products", getProducts);
+app.post("/api/v1/products", postProduct); */
+app.route("/api/v1/products").post(postProduct).get(getProducts);
+// 5th middleware
+app.use(responseTime());
+/* app.delete(`/api/v1/products/:productId`, deleteProduct);
 app.put(`/api/v1/products/:productId`, updateProduct);
-app.get(`/api/v1/products/:productId`, getProdct);
+app.get(`/api/v1/products/:productId`, getProdct); */
+app
+  .route("/api/v1/products/:productId")
+  .delete(deleteProduct)
+  .put(updateProduct)
+  .get(getProdct);
 app.listen(PORT, () => {
   console.log(`Listening to server on Port ${PORT}`);
 });
